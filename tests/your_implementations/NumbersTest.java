a3766efbd2e539177cae79dc83e25450d2d96e5b
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-11-04 18:38:39 +0100

    Create test cases for added methods

--- tests/your_implementations/NumbersTest.java
+++ tests/your_implementations/NumbersTest.java
@@ -4,7 +4,31 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class NumbersTest {
+public class NumbersTest {@Test
+
+    public void testIsArmstrongNumber() {
+        assertTrue(Numbers.isArmstrongNumber(0));
+        assertTrue(Numbers.isArmstrongNumber(1));
+        assertTrue(Numbers.isArmstrongNumber(153));
+        assertTrue(Numbers.isArmstrongNumber(9474));
+        assertFalse(Numbers.isArmstrongNumber(-153));
+        assertFalse(Numbers.isArmstrongNumber(10));
+        assertFalse(Numbers.isArmstrongNumber(123));
+        assertFalse(Numbers.isArmstrongNumber(1632));
+    }
+
+    @Test
+    public void testIsPalindrome() {
+        assertTrue(Numbers.isPalindrome(0)); 
+        assertTrue(Numbers.isPalindrome(121));
+        assertTrue(Numbers.isPalindrome(12321)); 
+        assertTrue(Numbers.isPalindrome(823328)); 
+        assertFalse(Numbers.isPalindrome(123));
+        assertFalse(Numbers.isPalindrome(-121));
+        assertFalse(Numbers.isPalindrome(12345));
+        assertFalse(Numbers.isPalindrome(123456));
+        assertFalse(Numbers.isPalindrome(-30085));
+    }
     
     @Test
     public void testReverseDigitsInNumber() {
@@ -49,5 +73,17 @@ public class NumbersTest {
         assertEquals(124, Numbers.getAbsoluteValue(-124));
         assertEquals(12345, Numbers.getAbsoluteValue(12345));
         assertEquals(2563, Numbers.getAbsoluteValue(-2563));
+    }
+
+    @Test
+    public void testToPowerOf() {
+        assertEquals(1, Numbers.toPowerOf(5, 0));
+        assertEquals(8, Numbers.toPowerOf(2, 3));
+        assertEquals(16, Numbers.toPowerOf(4, 2));
+        assertEquals(1024, Numbers.toPowerOf(2, 10));
+        assertEquals(1, Numbers.toPowerOf(10, 0));
+        assertThrows(IllegalArgumentException.class, () -> {
+            Numbers.toPowerOf(2, -3);
+        });
     }
 }
