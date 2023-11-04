20c1ba6f7ed2fe85a1a72950b8dbb89527afc70a
FrozenBirdXD <118717731+FrozenBirdXD@users.noreply.github.com>
2023-11-04 15:49:20 +0100

    Create short descriptions for methods

--- src/your_implementations/Numbers.java
+++ src/your_implementations/Numbers.java
@@ -7,22 +7,41 @@ public class Numbers {
         
     }
 
+    // Return the number with all the digits reversed.
+    // 12345 -> 54321
+    // If the number is negative:
+    // -12345 -> 54321
     public static int reverseDigitsInNumber(int number) {
         return -1;
     }
 
+    // Return the sum of all the digits in a number.
+    // 12345 -> 15
+    // If the number is negative:
+    // -12345 -> 15
     public static int sumOfDigitsInNumber(int number) {
         return -1;
     }
 
+    // Return the amount of digits in a number.
+    // 12345 -> 5
+    // If the number is negative:
+    // -12345 -> 5
     public static int digitsInNumber(int number) {
         return -1;
     }
 
+    // Return the rightmost digit in a number.
+    // 12346 -> 6
+    // If the number is negative:
+    // -12346 -> 6
     public static int getLastDigit(int number) {
         return -1;
     }
 
+    // Return the absolute Value of a number.
+    // 12345 -> 12345
+    // -12345 -> 12345
     public static int getAbsoluteValue(int number) {
         return -1;
     }
